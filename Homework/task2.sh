78e2e2e79e4fac7b961cea47397d7a75fe88c7ff
Marek Zarychta <marek_zarychta@epam.com>
2021-02-17 09:22:12 +0100

    Updated task2.sh

--- Homework/task2.sh
+++ Homework/task2.sh
@@ -1,65 +1,53 @@
 #!/bin/bash
-# Backup EPRT
-# ---------------------------------------------------------------------
-if [ $# -eq 0 ] ; then
-	echo "No arguments supplied"
-	exit 1
-fi
-echo $1
-if [ $1 -lt 1 ] ; then
-#if (( d < 1 )) ; then
-        echo "Invalid parameter - must be integer greater than 0"
-	exit 2
-fi
+# Backup script ---------------------------------------------------------------------
 
-OLDEST=$(date -d "now - ${1} days" +"%Y-%m-%d")
-TODAY=$(date +"%Y-%m-%d")
-SOURCE="/root/src"
-DEST="backup"
-[ -d $DEST ] || mkdir -p $DEST
-[ -d $DEST/$TODAY ] || mkdir -p $DEST/$TODAY
-echo "Creating backup: " $TODAY
-rsync -av --delete-during $SOURCE $DEST/$TODAY
-echo "Deleting backups older than: " $1 " days"
-echo "Oldest backup to keep: " $OLDEST
-cd $DEST
-# Iterate backup folder
-ls -d * | while read d
-do
-	if [ `dateutils.ddiff $d $TODAY` -gt $1 ] ; then
-		echo "Deleting backup: " $d
-		# rm -rf $d
-	fi
-done
-
-#!/bin/bash
-# Backup EPRT ---------------------------------------------------------------------
-backup_folder(){
- SRC="$1"
- DEST="$2"
- echo "Creating backup: " $TODAY
- rsync -av --delete-during $SOURCE $DEST/$TODAY
- echo "Deleting backups older than: " $1 " days"
- echo "Oldest backup to keep: " $OLDEST
-}
-cleanup(){
- cd $DEST
- # Iterate backup folder ls -d * | while read d
- for find in ...; do
- if [ `dateutils.ddiff $d $TODAY` -gt $1 ] ; then
- echo "Deleting backup: " $d
- # rm -rf $d
- fi
- done
-}
-if [ $# -eq 0 ] ; then
- echo "No arguments supplied"
- exit 1 fi echo $1 if [ $1 -lt 1 ] ; then
-#if (( d < 1 )) ; then
- echo "Invalid parameter - must be integer greater than 0"
- exit 2 fi OLDEST=$(date -d "now - ${1} days" +"%Y-%m-%d") TODAY=$(date +"%Y-%m-%d") SOURCE="/root/src" DEST="backup"
-# [ -d $DEST ] || mkdir -p $DEST
-[ -d $DEST/$TODAY ] || mkdir -p $DEST/$TODAY test ! -d "$DEST/$TODAY" && mkdir -p "$DEST/$TODAY"
-# TODO: Call functions TODO: check variables in functions/out functions 
+# TODO: Call functions TODO: check variables in functions/out functions
 # TODO: Better control on parameters
 # TODO: Better control during the execution *
+
+#Variables
+OLDEST=$(date -d "now - ${1} days" +"%Y-%m-%d")
+TODAY=$(date +"%Y-%m-%d")
+#Location to backup
+SOURCE="/root/src"
+#Backup destination
+DEST="backup"
+
+# Functions --------------------
+# Cleanup old backup function
+cleanup(){
+	cd $DEST
+        echo "Deleting backups older than: "$1" days"
+        echo "Oldest backup to keep: " $OLDEST
+	# Iterate backup folder ls -d * | while read d
+	for d in $(ls -d */); do
+		#If backup is older than $1 days - remove it
+		if [ `dateutils.ddiff $d $TODAY` -gt $1 ] ; then
+			echo "Deleting backup: " $d
+			# rm -rf $d
+		fi
+	done
+}
+
+#Backup function
+backup_folder(){
+	echo "Creating backup: " $TODAY
+	rsync -av --delete-during $SOURCE $DEST/$TODAY > /dev/null
+	echo "Backup done"
+}
+
+#Checking parameter
+checkparm(){
+	if [ $# -eq 0 ]; then
+		echo "No arguments supplied"
+		exit 1 
+	elif [ $1 -lt 1 ]; then
+		echo "Invalid parameter - must be integer greater than 0"
+		exit 2
+	fi
+}
+
+# Exectution -----------------
+checkparm $1
+backup_folder $1
+cleanup $1
